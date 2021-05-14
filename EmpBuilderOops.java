5c4105fad85522335920f8acd0cae2608cec2d93
nikita1803 <rainikita595@gmail.com>
2021-05-14 14:40:01 +0530

    Added: Check whether the employee is present or not

--- EmpBuilderOops.java
+++ EmpBuilderOops.java
@@ -2,7 +2,20 @@ package EmployeBuilderOops;
 
 public class EmpBuilderOops {
 	public static void main(String[] args) {
-	System.out.println("Welcome in Employee Builder through OOPS concept");
+		
+		EmpBuilderOops emp = new EmpBuilderOops();
+		emp.Attendence();
+		}
+	void Attendence()
+	{
+		double attendance = Math.floor(Math.random() * 10) % 2; 
+		if(attendance==1) {
+		  System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
 	}
 
+	
 }
