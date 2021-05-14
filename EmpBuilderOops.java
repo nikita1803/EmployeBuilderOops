a26baa6f809d78d714bb616059001fe3fe063f0b
nikita1803 <rainikita595@gmail.com>
2021-05-14 12:01:05 +0530

    Welcome in Employee Builder through OOPS concept

---
+++ EmpBuilderOops.java
@@ -0,0 +1,8 @@
+package EmployeBuilderOops;
+
+public class EmpBuilderOops {
+	public static void main(String[] args) {
+	System.out.println("Welcome in Employee Builder through OOPS concept");
+	}
+
+}
