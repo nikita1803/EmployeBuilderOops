2c2d9ccdc32662983d52bf7376c6d75d09f73791
nikita1803 <rainikita595@gmail.com>
2021-05-14 19:08:43 +0530

    Added: refactor and update the code of employee wage by using oops concept

--- EmpBuilderOops.java
+++ EmpBuilderOops.java
@@ -1,62 +1,60 @@
 package EmployeBuilderOops;
 
 
-public class EmpBuilderOops 
+class EmpBuilderOops 
 {
-	
 	int isFullTime = 2;
 	int isPartime = 1;
 	int wagePerHours=20;
-	int numWorkDays=20;
 	int totalEmpHours=0;
 	int workHours=0;
 
-
-	public static void main(String[] args) 
-	{
-		
-		EmpBuilderOops emp = new EmpBuilderOops();
-		int ans = emp.Attendence();
-		int workHours= 0;
-		int wage= 0;
+int Attendence()
+{
+	int hrsInMonth=100;
+	int workDays=0;
+	int totalEmpHours=0;
+	int numWorkDays=20;
 	
-		emp.Wages(ans);
-		
-	}
-	int Attendence()
+	while(workHours <= hrsInMonth && workDays <= numWorkDays)
 	{
-		int hrsInMonth=100;
-		int workDays=0;
-		int totalEmpHours=0;
+		workDays++ ;
 		
-
-		while(workHours <= hrsInMonth && workDays <= numWorkDays)
+		int attendance = (int) Math.floor(Math.random() * 10) % 3; 
+		switch (attendance)
 		{
-			workDays++ ;
-			
-			int attendance = (int) Math.floor(Math.random() * 10) % 3; 
-			switch (attendance){
-			case 1: 
-				workHours=workHours+8;
+		case 1: 
+			workHours=workHours+8;
+		break;
+		case 2: 
+			workHours=workHours+4;
 			break;
-			case 2: 
-				workHours=workHours+4;
-				break;
-			case 3: 
-				workHours = workHours+0;
-			}
-			//totalEmpHours = (workHours + totalEmpHours);
-			System.out.println("Total Working hours :");
-			System.out.println (workHours);
+		case 3: 
+			workHours = workHours+0;
 		}
-		return workHours;
+		System.out.println("Total Working hours :");
+		System.out.println (workHours);
+	}
+	return workHours;
+}
 
-	}
-	void Wages(int workHours)
-	{
-		
-		int totalSalary = ( wagePerHours * workHours);
-		System.out.println("Total Wages for a working hours and days :");
-		System.out.println (totalSalary);
-	}
+void Wages(int workHours)
+{
+	
+	int totalSalary = ( wagePerHours * workHours);
+	System.out.println("Total Wages for a working hours and days :");
+	System.out.println (totalSalary);
+}
+
+public static void main(String[] args) 
+{
+	
+	EmpBuilderOops emp = new EmpBuilderOops();
+	int ans = emp.Attendence();
+	int workHours= 0;
+	int wage= 0;
+
+	emp.Wages(ans);
+	
+}
 }
