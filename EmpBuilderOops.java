5fd3eb1f9d518d842b6b72240bb7afae55d7a16d
nikita1803 <rainikita595@gmail.com>
2021-05-14 15:45:01 +0530

    Added: Calculated part time and full time wages of the employee

--- EmpBuilderOops.java
+++ EmpBuilderOops.java
@@ -1,8 +1,10 @@
 package EmployeBuilderOops;
 
 public class EmpBuilderOops {
-	int workHours=8;
+	int isFullTime = 2;
+	int isPartime = 1;
 	int wagePerHours=20;
+//	int workHours=0;
 
 	public static void main(String[] args) {
 		
@@ -10,27 +12,38 @@ public class EmpBuilderOops {
 		int ans = emp.Attendence();
 		if(ans==1)
 		{
-			int wadge=emp.Wadges();
-			System.out.println("Daily Wages of the employee is : " +wadge);
+			int workHours=8;
+			int wadge=emp.Wadges(workHours);
+			System.out.println("Full time Wages of the employee is : " +wadge);
 		}
-		else
+		else if(ans==2)
 		{
+			int workHours=4;
+			int wadge=emp.Wadges(workHours);
+			System.out.println("Part time Wages of the employee is : " +wadge);
+		}
+		else {
 			System.out.println("Employee is absent");
 		}
-		}
+	}
 	int Attendence()
 	{
-		double attendance = Math.floor(Math.random() * 10) % 2; 
+		double attendance = Math.floor(Math.random() * 10) % 3; 
 		if(attendance==1) {
 		  return 1;
 		}
-		else {
+		else if (attendance==2){
+			return 2;
+		}
+		else
+		{
 			return 0;
 		}
 	}
-	int Wadges()
+	int Wadges(int workHours)
 	{	
 		int dailyWage = (wagePerHours * workHours);
 		return dailyWage;
 	}
 }
+
